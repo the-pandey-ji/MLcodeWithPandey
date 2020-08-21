8c0234e9acae198c112d7108095598f8a7526f40
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-08-21 10:49:00 +0530

    update

--- classification.py
+++ classification.py
@@ -8,7 +8,7 @@ print(iris.data[0], iris.target[0])
 features = iris.data
 labels = iris.target
 
-clf = KNeighborsClassifier
+clf = KNeighborsClassifier()
 clf.fit(features, labels)
 
 prediction = clf.predict([[1, 1, 2, 3]])
